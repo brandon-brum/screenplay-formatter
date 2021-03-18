cc7ac154fe562174f256852d5f3a8cb62cbdb319
Brandon Brum <BrandonBrum@gmail.com>
2021-03-17 23:27:57 -0600

    small tweaks

--- scripts/formatting.js
+++ scripts/formatting.js
@@ -1,7 +1,7 @@
 const letterSize = {
     width:8.5,
     height:11
-}
+};
 
 class format {
     constructor(leftMargin, rightMargin) {
@@ -9,13 +9,15 @@ class format {
         this.rightMargin = rightMargin;
         this.width = letterSize.width - (leftMargin + rightMargin);
     }
-}
+};
 
 let formats = {
     pageIndicator: new format(7.5, 0),
     action: new format(1.5, 1),
+    heading: new format(1.5, 1),
     dialogue: new format(2.9, 2.3),
     cue: new format(4.2, 1),
     parenthetical: new format(3.6, 2.9),
-    transition: new format(6, 1)
-}
+    transition: new format(6, 1),
+    inTransition: new format(1.5, 1), // Fade ins are put on the action margin.
+};
--- scripts/screenPlay.class.js
+++ scripts/screenPlay.class.js
@@ -1,6 +1,7 @@
 class screenPlay {
     constructor(raw='', characters='') {
-        this.lines = raw.split('\n');
+        this.lines = raw.split('\n')
+            .filter(line => line); // Removes empty lines
         this.characters = characters.split('\n')
     }
     createPDF() {
@@ -12,28 +13,23 @@ class screenPlay {
             lineHeight: 1
         });
         this.doc.setFontSize(11);
-        this.doc.setFont('Courier');
+        this.doc.setFont("Courier", "", "bold");
         this.pageNumber = 1;
         this.lineNumber = 1;
         
         this.write.pageIndicator();
         this.lines.map(line => {
-            //console.log(line)
-            if (this.lineNumber > 55) {
-                this.write.pageIndicator();
-                this.pageNumber++
-                this.doc.addPage('letter', 'p');
-                this.lineNumber = 1;  
-            }
-            let linePurpose = this.getLinePurpose(line)
-            let textHeight = this.doc.splitTextToSize(line, formats[linePurpose].width).length
-            console.log(line, formats.transition.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.transition.width });
+            let linePurpose = this.getLinePurpose(line);
+            let textHeight = this.doc.splitTextToSize(line, formats[linePurpose].width).length;
+            if (['cue','action','transition'].includes(linePurpose))
+                this.lineNumber++;
+            if (this.lineNumber > 55)
+                this.add.page();
             this.write[linePurpose](line);
-            
             this.lineNumber += textHeight;
         });
         document.querySelector('a').href = URL.createObjectURL(this.doc.output('blob'));
-        document.querySelector('a').style.display = 'block'
+        document.querySelector('a').style.display = 'block';
     }
 
     getLinePurpose(line) {
@@ -44,7 +40,7 @@ class screenPlay {
             return 'parenthetical';
         } else if (/^(\S* )*\w*:$/.test(line)) {
             this.afterCue = false;
-            return 'transition';
+            return line.toUpperCase().includes('IN') ? 'inTransition' : 'transition';
         } else if (this.afterCue) {// Any blocks of text after character cues are assumed to be dialogue.
             this.afterCue = false ;
             return 'dialogue';
@@ -56,12 +52,22 @@ class screenPlay {
     corrections = {};
 
     write = {
-        pageIndicator: () => this.doc.text(`${this.pageNumber}`, formats.pageIndicator.leftMargin, 0.5, { align: 'right' }),
+        pageIndicator: () => this.doc.text(`${this.pageNumber}.`, formats.pageIndicator.leftMargin, 0.5, { align: 'right' }),
         action: text => this.doc.text(text, formats.action.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.action.width }),
         dialogue: text => this.doc.text(text, formats.dialogue.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.dialogue.width }),
         cue: text => this.doc.text(text, formats.cue.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.cue.width }),
         parenthetical: text => this.doc.text(text, formats.parenthetical.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.parenthetical.width }),
-        transition: text => this.doc.text(text, formats.transition.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.transition.width })
+        transition: text => this.doc.text(text, formats.transition.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.transition.width }),
+        inTransition: text => this.doc.text(text, formats.inTransition.leftMargin, this.lineOffset * this.lineNumber + 1, { maxWidth:formats.inTransition.width })
+    };
+
+    add = {
+        page: () => {
+            this.write.pageIndicator();
+            this.pageNumber++;
+            this.doc.addPage('letter', 'p');
+            this.lineNumber = 1;
+        },
     };
 
     get raw() {return this.lines.join('\n')}
